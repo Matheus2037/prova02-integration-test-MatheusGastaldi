2823300be9dbe631dd5afaca15dfd7a329043ff5
Matheus Da Silva Gastaldi <matheus.30220@alunosatc.edu.br>
2025-09-22 20:25:07 -0300

    Ajuste para nao barrar no cloudfare

--- test/fake_store.spec.ts
+++ test/fake_store.spec.ts
@@ -18,6 +18,9 @@ describe('Fake Store API', () => {
       await p
         .spec()
         .get(`${baseUrl}/products/1`)
+        .withHeaders({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+        })
         .expectStatus(StatusCodes.OK)
         .expectBodyContains("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops")
     });
