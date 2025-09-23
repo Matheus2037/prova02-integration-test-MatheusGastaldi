c834d0fa68ded6a8467684fda5a2c0ddf679e4f3
Matheus Da Silva Gastaldi <matheus.30220@alunosatc.edu.br>
2025-09-22 21:02:19 -0300

    Trocando de API por conta da CLoudfare

--- test/fake_store.spec.ts
+++ test/fake_store.spec.ts
@@ -3,7 +3,7 @@ import { SimpleReporter } from '../simple-reporter';
 import { StatusCodes } from 'http-status-codes';
 
 describe('Fake Store API', () => {
-  const baseUrl = 'https://fakestoreapi.com';
+  const baseUrl = 'https://gorest.co.in/public/v2';
   const p = pactum;
   const rep = SimpleReporter;
 
@@ -13,16 +13,13 @@ describe('Fake Store API', () => {
   afterAll(() => p.reporter.end());
 
 
-  describe('Get Product By ID', () => {
+  describe('Get Booker By ID', () => {
     it('Product', async () => {
       await p
         .spec()
-        .get(`${baseUrl}/products/1`)
-        .withHeaders({
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-        })
+        .get(`${baseUrl}/users/7440571`)
         .expectStatus(StatusCodes.OK)
-        .expectBodyContains("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops")
+        .expectBodyContains("Amrit Gowda")
     });
   });
 });
